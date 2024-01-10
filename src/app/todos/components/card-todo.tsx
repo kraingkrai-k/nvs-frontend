2eb4b1aa9df9e3a3e4d166fcae85f82a4fcc8960
km <km@kms-MacBook-Air.local>
2024-01-10 12:10:00 +0700

    refactor: remove unused

--- src/app/todos/components/card-todo.tsx
+++ src/app/todos/components/card-todo.tsx
@@ -62,9 +62,7 @@ const CardTodo = (props: ICardTodo) => {
           </button>
         </p>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-          {/* {card.description} */}
+          {card.description}
         </p>
       </div>
     </>
